653bea190a616d18857e2872844fb071812a2416
James Park <87226158+JamesPark89@users.noreply.github.com>
2022-08-11 16:22:40 +1000

    Update jobs.js
    Change method
    findById
    => findOneAndUpdate

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -3,6 +3,7 @@ let Job = require("../models/jobs.model")
 
 router.route("/").get((req, res) => {
   Job.find()
+    // .populate("author")
     .limit(20)
     .sort({ jobId: -1 })
     .then((jobs) => res.json(jobs))
@@ -14,11 +15,13 @@ router.route("/add").post((req, res) => {
   const title = req.body.title
   const contents = req.body.contents
   const date = Date.parse(req.body.date)
+  // const author = req.user.id
   const newJob = new Job({
     username,
     title,
     contents,
     date
+    // author
   })
 
   newJob
@@ -29,6 +32,7 @@ router.route("/add").post((req, res) => {
 
 router.route("/:id").get((req, res) => {
   Job.findById(req.params.id)
+    // .populate("author")
     .then((exercises) => res.json(exercises))
     .catch((err) => res.status(400).json("Error:" + err))
 })
@@ -40,21 +44,14 @@ router.route("/:id").delete((req, res) => {
 })
 
 router.route("/update/:id").post((req, res) => {
-  Job.findById(req.params.id)
-    .then((jobs) => {
-      jobs.username = req.body.username
-      jobs.title = req.body.title
-      jobs.contents = req.body.contents
-      jobs.date = Date.parse(req.body.date)
-
-      jobs
-        .save()
-        .then(() => res.json("jobs updated!"))
-        .catch((err) =>
-          res.status(400).json("Error:" + err)
-        )
-    })
-    .catch((err) => res.status(400).json("Error:" + err))
+  Job.findOneAndUpdate(
+    { _id: req.params.id },
+    req.body,
+    (err, jobs) => {
+      if (err) return res.json(err)
+      res.redirect("/job")
+    }
+  )
 })
 
 module.exports = router
