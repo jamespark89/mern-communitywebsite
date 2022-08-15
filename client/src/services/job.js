3a25714c665d3b5d935a4b42013a2b4a77197063
James Park <87226158+JamesPark89@users.noreply.github.com>
2022-08-15 14:13:19 +1000

    Update api URL
    Update api URL

--- client/src/services/job.js
+++ client/src/services/job.js
@@ -2,25 +2,27 @@ const axios = require("axios")
 axios.defaults.withCredentials = true
 class jobDataService {
   async getAll() {
-    return await axios.get("http://localhost:3001/jobs")
+    return await axios.get("http://localhost:3001/api/jobs")
   }
   getById(id) {
-    return axios.get(`http://localhost:3001/jobs/${id}`)
+    return axios.get(`http://localhost:3001/api/jobs/${id}`)
   }
   createJob(data) {
     return axios.post(
-      "http://localhost:3001/jobs/add",
+      "http://localhost:3001/api/jobs/",
       data
     )
   }
   updateJob(data, id) {
-    return axios.post(
-      `http://localhost:3001/jobs/update/${id}`,
+    return axios.put(
+      `http://localhost:3001/api/jobs/${id}`,
       data
     )
   }
   deleteJob(id) {
-    return axios.delete(`http://localhost:3001/jobs/${id}`)
+    return axios.delete(
+      `http://localhost:3001/api/jobs/${id}`
+    )
   }
 }
 export default new jobDataService()
