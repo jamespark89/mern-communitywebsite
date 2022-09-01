61a18ed960399b6c0ab15e52287d2789376c676a
James Park <87226158+JamesPark89@users.noreply.github.com>
2022-09-02 00:22:59 +1000

    2nd deploy with s3

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,11 @@
 import GoogleButton from "react-google-button"
 
 export default function Login() {
-  const SERVER_URL = "http://localhost:4000"
   const handleGoogleLogin = async () => {
-    window.open(`${SERVER_URL}/auth/google/login`, "_self")
+    window.open(
+      `${process.env.REACT_APP_SERVER_URL}/auth/google/login`,
+      "_self"
+    )
   }
   return (
     <div className="flex justify-center pt-20 h-screen">
