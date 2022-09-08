333c028f72725e0d81922cec540452e261929459
James Park <87226158+JamesPark89@users.noreply.github.com>
2022-09-08 12:57:49 +1000

    Update Pagination
    -Update for multiple pages( totalPage>8)

--- client/src/components/Pagination.js
+++ client/src/components/Pagination.js
@@ -7,16 +7,33 @@ function Pagination({
   totalHouseNumber
 }) {
   const totalPage = Math.ceil(totalHouseNumber / limit)
-  const pages = []
-  for (let i = 0; i < totalPage; i++) {
-    pages.push(i)
-  }
   function classNames(...classes) {
     return classes.filter(Boolean).join(" ")
   }
   return (
-    <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-      <div className="flex flex-1 justify-between sm:hidden"></div>
+    <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6 mt-5">
+      <div className="flex flex-1 justify-between sm:hidden">
+        <Link
+          to={
+            currentPage > 1
+              ? `?page=${currentPage - 1}`
+              : `?page=1`
+          }
+          class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+        >
+          Previous
+        </Link>
+        <Link
+          to={
+            currentPage < totalPage
+              ? `?page=${currentPage + 1}`
+              : `?page=${totalPage}`
+          }
+          class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+        >
+          Next
+        </Link>
+      </div>
       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
         <div>
           <p className="text-sm text-gray-700">
@@ -45,22 +62,103 @@ function Pagination({
               <span className="sr-only">Previous</span>
               {"<"}
             </Link>
-            {pages.map((value, index) => (
-              <NavLink
-                key={index}
-                to={`?page=${index + 1}`}
-                className={() =>
-                  classNames(
-                    currentPage === index + 1
-                      ? "border-indigo-500 bg-indigo-50 text-indigo-600 focus:z-20"
-                      : "bg-white border-gray-300 text-gray-500 hover:bg-gray-50",
-                    "relative z-10 inline-flex items-center border px-4 py-2 text-sm font-medium"
-                  )
-                }
-              >
-                {index + 1}
-              </NavLink>
-            ))}
+            {totalPage < 8 ? (
+              Array(totalPage)
+                .fill(null)
+                .map((value, index) => (
+                  <NavLink
+                    key={index}
+                    to={`?page=${index + 1}`}
+                    className={() =>
+                      classNames(
+                        currentPage === index + 1
+                          ? "border-indigo-500 bg-indigo-50 text-indigo-600 focus:z-20"
+                          : "bg-white border-gray-300 text-gray-500 hover:bg-gray-50",
+                        "relative z-10 inline-flex items-center border px-4 py-2 text-sm font-medium"
+                      )
+                    }
+                  >
+                    {index + 1}
+                  </NavLink>
+                ))
+            ) : (
+              <>
+                {currentPage < 3 ? null : (
+                  <>
+                    <NavLink
+                      to={`?page=1`}
+                      className={() =>
+                        classNames(
+                          currentPage === 1
+                            ? "border-indigo-500 bg-indigo-50 text-indigo-600 focus:z-20"
+                            : "bg-white border-gray-300 text-gray-500 hover:bg-gray-50",
+                          "relative z-10 inline-flex items-center border px-4 py-2 text-sm font-medium"
+                        )
+                      }
+                    >
+                      {1}
+                    </NavLink>
+                    <span class="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700">
+                      ...
+                    </span>
+                  </>
+                )}
+
+                {Array(3)
+                  .fill(null)
+                  .map((value, index) => (
+                    <NavLink
+                      key={index}
+                      to={`?page=${
+                        currentPage >= totalPage
+                          ? totalPage - 2 + index
+                          : currentPage < 3
+                          ? index + 1
+                          : currentPage - 1 + index
+                      }`}
+                      className={() =>
+                        classNames(
+                          currentPage ===
+                            (currentPage >= totalPage
+                              ? totalPage - 2 + index
+                              : currentPage < 3
+                              ? index + 1
+                              : currentPage - 1 + index)
+                            ? "border-indigo-500 bg-indigo-50 text-indigo-600 focus:z-20"
+                            : "bg-white border-gray-300 text-gray-500 hover:bg-gray-50",
+                          "relative z-10 inline-flex items-center border px-4 py-2 text-sm font-medium"
+                        )
+                      }
+                    >
+                      {currentPage >= totalPage
+                        ? totalPage - 2 + index
+                        : currentPage < 3
+                        ? index + 1
+                        : currentPage - 1 + index}
+                    </NavLink>
+                  ))}
+                {currentPage < totalPage - 1 ? (
+                  <>
+                    <span class="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700">
+                      ...
+                    </span>
+                    <NavLink
+                      to={`?page=${totalPage}`}
+                      className={() =>
+                        classNames(
+                          currentPage === totalPage
+                            ? "border-indigo-500 bg-indigo-50 text-indigo-600 focus:z-20"
+                            : "bg-white border-gray-300 text-gray-500 hover:bg-gray-50",
+                          "relative z-10 inline-flex items-center border px-4 py-2 text-sm font-medium"
+                        )
+                      }
+                    >
+                      {totalPage}
+                    </NavLink>
+                  </>
+                ) : null}
+              </>
+            )}
 
             <Link
               to={
