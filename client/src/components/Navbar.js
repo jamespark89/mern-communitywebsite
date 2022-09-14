2642780e52524d46cac84faa6ccf57f9e3eff319
James Park <87226158+JamesPark89@users.noreply.github.com>
2022-09-14 22:24:09 +1000

    Update a tag to NavLink
    Update a tag to NavLink

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -115,8 +115,8 @@ export default function Navbar() {
                         <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                           <Menu.Item>
                             {({ active }) => (
-                              <a
-                                href={`/profile/${user?._id}`}
+                              <NavLink
+                                to={`/profile/${user?._id}`}
                                 className={classNames(
                                   active
                                     ? "bg-gray-100"
@@ -125,7 +125,7 @@ export default function Navbar() {
                                 )}
                               >
                                 Your Profile
-                              </a>
+                              </NavLink>
                             )}
                           </Menu.Item>
 
